34ef797a7af3d02aa1e5254ff662934e7e4000f0
Nouman Ahmed <35970677+nouman-10@users.noreply.github.com>
2022-02-23 02:58:58 +0100

    Update App.js

--- react-time-picker/src/App.js
+++ react-time-picker/src/App.js
@@ -7,7 +7,24 @@ import TimePicker from '@mui/lab/TimePicker';
 import DateTimePicker from '@mui/lab/DateTimePicker';
 import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
 import MobileDatePicker from '@mui/lab/MobileDatePicker';
- 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { purple } from '@mui/material/colors';
+
+const theme = createTheme({
+  typography: {
+    fontSize: 12,
+    fontFamily: "Segoe UI, Helvetica, Arial, sans-serif",
+  },
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: '#f44336',
+    },
+  },
+});
+
 export default function App() {
   const [value, setValue] = React.useState(new Date('2014-08-18T21:11:54'));
  
@@ -16,37 +33,39 @@ export default function App() {
   };
  
   return (
-    <div style={{margin: "5% 40%"}}>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <Stack spacing={3}>
-          <DesktopDatePicker
-            label="Date desktop"
-            inputFormat="MM/dd/yyyy"
-            value={value}
-            onChange={handleChange}
-            renderInput={(params) => <TextField {...params} />}
-          />
-          <MobileDatePicker
-            label="Date mobile"
-            inputFormat="MM/dd/yyyy"
-            value={value}
-            onChange={handleChange}
-            renderInput={(params) => <TextField {...params} />}
-          />
-          <TimePicker
-            label="Time"
-            value={value}
-            onChange={handleChange}
-            renderInput={(params) => <TextField {...params} />}
-          />
-          <DateTimePicker
-            label="Date&Time picker"
-            value={value}
-            onChange={handleChange}
-            renderInput={(params) => <TextField {...params} />}
-          />
-        </Stack>
-      </LocalizationProvider>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div style={{margin: "5% 40%"}}>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <Stack spacing={3}>
+            <DesktopDatePicker
+              label="Date desktop"
+              inputFormat="MM/dd/yyyy"
+              value={value}
+              onChange={handleChange}
+              renderInput={(params) => <TextField {...params} />}
+            />
+            <MobileDatePicker
+              label="Date mobile"
+              inputFormat="MM/dd/yyyy"
+              value={value}
+              onChange={handleChange}
+              renderInput={(params) => <TextField {...params} />}
+            />
+            <TimePicker
+              label="Time"
+              value={value}
+              onChange={handleChange}
+              renderInput={(params) => <TextField {...params} />}
+            />
+            <DateTimePicker
+              label="Date&Time picker"
+              value={value}
+              onChange={handleChange}
+              renderInput={(params) => <TextField {...params} />}
+            />
+          </Stack>
+        </LocalizationProvider>
+      </div>
+    </ThemeProvider>
   );
 }
